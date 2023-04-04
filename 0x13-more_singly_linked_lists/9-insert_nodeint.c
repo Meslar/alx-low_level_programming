b29e1c687280790e6cfa13af06532d31aef1ae84
Meslar <meshach.laryea@gmail.com>
2023-04-04 15:01:22 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tp_node->next = new_node;
 			return (new_node);
 		}
-		tp_node = tmp_node->next;
+		tp_node = tp_node->next;
 		count++;
 	}
 	return (NULL);
