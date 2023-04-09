73e4e24a88b3f023634081dce53658f3306ed535
Meslar <meshach.laryea@gmail.com>
2023-04-09 14:03:38 -0700

    Textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read = 0;
 	char *buffer = malloc(sizeof(char) * (letters + 1));
 	int fd;
-	
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	
+
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -27,9 +27,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	
+
 	buffer[bytes_read] = '\0';
-	
+
 	if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
 	{
 		close(fd);
