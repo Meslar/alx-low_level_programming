427b9b778b59e5204f2441684faabfa7d6927cb1
Meslar <meshach.laryea@gmail.com>
2023-04-09 05:33:59 -0700

    Clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n &=~(1UL << index);
+	*n &= ~(1UL << index);
 
 	return (1);
 }
